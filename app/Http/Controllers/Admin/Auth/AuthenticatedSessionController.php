dd00d9b4c9303f42ee713c9766d66c5b0a7122f9
budou114 <58349030+budou114@users.noreply.github.com>
2021-12-12 15:10:10 +0900

    変更間違いの修正

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -43,7 +43,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request)
     {
-        Auth::guard('admin')->logout();
+        Auth::guard('admins')->logout();
 
         $request->session()->invalidate();
 
