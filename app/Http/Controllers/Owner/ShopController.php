d95b8bb4aab968bbc7613adeb053a5a03634e2bc
budou114 <58349030+budou114@users.noreply.github.com>
2021-12-19 13:34:41 +0900

    indexアクションの内容を追加

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
 
 class ShopController extends Controller
 {
@@ -14,6 +16,10 @@ class ShopController extends Controller
 
     public function index()
     {
+        $owner_id = Auth::id();
+        $shops = Shop::where('owner_id', $owner_id)->get();
+
+        return view('owner.shops.index', compact('shops'));
     }
 
     public function edit($id)
