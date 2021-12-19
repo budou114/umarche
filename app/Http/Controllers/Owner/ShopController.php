fc94e378f0b140762b205e201460eba85f0132e7
budou114 <58349030+budou114@users.noreply.github.com>
2021-12-19 16:07:43 +0900

    フォームリクエスト機能の追加

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 
 class ShopController extends Controller
 {
@@ -41,7 +42,7 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $image_file = $request->image;
         if(!is_null($image_file) && $image_file->isValid()) {
