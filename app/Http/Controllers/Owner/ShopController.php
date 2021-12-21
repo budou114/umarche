3968318f46bc1d9ac3adc790d821f9b809e0f5e3
budou114 <58349030+budou114@users.noreply.github.com>
2021-12-21 12:04:27 +0900

    バリデーションなどの更新機能を追加

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -45,11 +45,30 @@ class ShopController extends Controller
 
     public function update(UploadImageRequest $request, $id)
     {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'information' => ['required', 'string', 'max:1000'],
+            'is_selling' => ['required'],
+        ]);
+
         $image_file = $request->image;
         if(!is_null($image_file) && $image_file->isValid()) {
             $file_name_to_store = ImageService::upload($image_file, 'shops');
         }
 
-        return redirect()->route('owner.shops.index');
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+        if(!is_null($image_file) && $image_file->isValid()) {
+            $shop->filename = $file_name_to_store;
+        }
+
+        $shop->save();
+
+        return redirect()
+        ->route('owner.shops.index')
+        ->with(['message' =>'店舗情報を更新しました。',
+        'status' => 'info']);
     }
 }
