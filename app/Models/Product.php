ca8e993fc5286728e26a8585d97a720bbe6f9375
budou114 <58349030+budou114@users.noreply.github.com>
2021-12-22 12:28:41 +0900

    リレーションの設定

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Shop;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
 }
