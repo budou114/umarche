ef890fec6aa7581d0daa336e9bac538c31d0713c
budou114 <58349030+budou114@users.noreply.github.com>
2022-01-09 12:31:52 +0900

    フラッシュメッセージの追加とエラー個所の修正

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                    <x-flash-message status="session('status')" />
                     <form method="post" action="{{ route('owner.products.update', ['product' => $product->id])}}">
                         @csrf
                         @method('put')
@@ -41,7 +42,7 @@
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                     <label for="current_quantity" class="leading-7 text-sm text-gray-600">現在の在庫※必須</label>
-                                    <input type="hidden" id="current_quantity" name="current_quantity" required  value="{{ $product->quantity }}">
+                                    <input type="hidden" id="current_quantity" name="current_quantity" required  value="{{ $quantity }}">
                                     <div class="w-full bg-gray-100 bg-opacity-50 rounded -700 py-1 px-3 leading-8">{{ $quantity }}</div>
                                 </div>
                             </div>
