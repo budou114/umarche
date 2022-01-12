f35346122b28db95cd3b5da6b3df27da88c3b7b5
budou114 <58349030+budou114@users.noreply.github.com>
2022-01-12 11:53:54 +0900

    画面にページネーションを追加

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -37,7 +37,26 @@
                                 </option>
                             </select>
                         </div>
-                        <div>表示件数</div>
+                        <div>
+                            <span>表示件数</span><br>
+                            <select id="pagination" name="pagination">
+                                <option value="20"
+                                    @if(\Request::get('pagination') === '20')
+                                    selected
+                                    @endif>20件
+                                </option>
+                                <option value="50"
+                                    @if(\Request::get('pagination') === '50')
+                                    selected
+                                    @endif>50件
+                                </option>
+                                <option value="100"
+                                    @if(\Request::get('pagination') === '100')
+                                    selected
+                                    @endif>100件
+                                </option>
+                            </select>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -64,6 +83,7 @@
                             </div>
                         @endforeach
                     </div>
+                    {{ $products->appends(['sort' => \Request::get('sort'), 'pagination' => \Request::get('pagination')])->links() }}
                 </div>
             </div>
         </div>
@@ -73,5 +93,11 @@
         select.addEventListener('change', function(){
             this.form.submit();
         });
+        
+        const paginate = document.getElementById('pagination')
+        paginate.addEventListener('change', function(){
+            this.form.submit();
+        });
+
     </script>
 </x-app-layout>
